f428a5b2078c4e941ea6f96eeec10582eb8b5546
William Chong <williamchong007@msn.com>
2020-02-20 15:55:48 +0800

    ➕ Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,18 @@ module.exports = {
     library: 'likePay',
     libraryTarget: 'umd',
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
