128fc4e9b298b8968a0abde18dfdb41dc74c7837
William Chong <williamchong007@msn.com>
2020-02-22 01:27:49 +0800

    ✨ Add avatar in qr code

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,29 @@ export async function pollForTxComplete(txId: string) {
   return txData;
 }
 
+function drawAvatarInQRCode(canvas: HTMLCanvasElement, avatarSrc: string) {
+  const context = canvas.getContext('2d');
+  const image = new Image();
+  image.onload = () => {
+    const imageWidth = canvas.width / 5;
+    const imageHeight = canvas.height / 5;
+    const canvasWidthCenter = canvas.width / 2;
+    const canvasHeightCenter = canvas.height / 2;
+    context.save();
+    context.arc(canvasWidthCenter, canvasHeightCenter, imageWidth / 2, 0, Math.PI * 2);
+    context.clip();
+    context.drawImage(
+      image,
+      canvasWidthCenter - imageWidth / 2,
+      canvasHeightCenter - imageHeight / 2,
+      imageWidth,
+      imageHeight,
+    );
+    context.restore();
+  };
+  image.src = avatarSrc;
+}
+
 export async function createPaymentQRCode(
   selector: string,
   likerId: string,
@@ -33,7 +56,7 @@ export async function createPaymentQRCode(
   { blocking = true } = {},
 ) {
   const user = await getUserInfo(likerId);
-  const { cosomosWallet } = user;
+  const { cosomosWallet, avatar } = user;
   const cosmosAmount = {
     denom: COSMOS_DENOM,
     amount: new BigNumber(amount).multipliedBy(1e9).toFixed(),
@@ -44,7 +67,17 @@ export async function createPaymentQRCode(
     amount: cosmosAmount,
     memo: uuid,
   });
-  await QRCode.toCanvas(document.querySelector(selector), payload);
+  const canvas = <HTMLCanvasElement>document.querySelector(selector);
+  // TODO: check canvas is canvas
+  await QRCode.toCanvas(canvas, payload, {
+    errorCorrectionLevel: 'H',
+    color: { dark: '#28646e' },
+  });
+  try {
+    if (avatar) drawAvatarInQRCode(canvas, avatar);
+  } catch (err) {
+    console.error(err);
+  }
   const txId = uuid; // TODO: encode with base64/protobuf?
   if (!blocking) return { id: txId };
   const txData = await pollForTxComplete(txId);
