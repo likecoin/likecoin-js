9337def42bae9b98389babc261cdaaece733e3d2
William Chong <me@williamchong.cloud>
2021-11-09 01:51:47 +0800

    ✨ Add submit arweave iscn function

--- src/arweave/index.ts
+++ src/arweave/index.ts
@@ -1,4 +1,32 @@
-import { estimateArweavePrice, uploadArweavePrice } from './api';
-import ISCNSigningClient from '../iscn';
+import { estimateArweavePrice, uploadToArweave } from './api';
+import signingClient from '../iscn';
 import { sendLIKE } from '../tx';
 
+export async function submitToArweaveAndISCN(
+  files: string | File[],
+  iscnMetadata,
+  signer,
+  fromAddress,
+) {
+  const estimate = await estimateArweavePrice(files);
+  const {
+    arweaveId: existingArweaveId,
+    address,
+    memo,
+    LIKE,
+  } = estimate;
+  let arweaveId = existingArweaveId;
+  if (!arweaveId) {
+    const res = await sendLIKE(fromAddress, address, LIKE, signer, memo);
+    ({ arweaveId } = await uploadToArweave(files, res.transactionHash));
+  }
+  let { contentFingerprints = [] } = iscnMetadata;
+  if (!contentFingerprints) contentFingerprints = [];
+  contentFingerprints.push(`ar://${arweaveId}`);
+  const iscnMetadataWithArweaveId = { ...iscnMetadata, contentFingerprints };
+  await signingClient.setSigner(signer);
+  const res = await signingClient.createISCNRecord(fromAddress, iscnMetadataWithArweaveId);
+  return res;
+}
+
+export default submitToArweaveAndISCN;
--- src/iscn/index.ts
+++ src/iscn/index.ts
@@ -1,3 +1,3 @@
 import { ISCNSigningClient } from '@likecoin/iscn-js';
 
-export default ISCNSigningClient;
+export default new ISCNSigningClient();
