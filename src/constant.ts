c8c232f71d3d9fea56bb7eebde61843348e60428
William Chong <me@williamchong.cloud>
2021-11-09 00:52:33 +0800

    ✨ Add iscn and tx code

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,8 @@
 export const LIKE_CO_API = 'https://api.like.co/';
 export const LIKE_CO_TEST_API = 'https://api.rinkeby.like.co/';
+
+export const COSMOS_DENOM = 'nanolike';
+export const TRANSFER_GAS = 100000;
+export const DEFAULT_GAS_PRICE = [{ amount: 1000, denom: COSMOS_DENOM }];
+export const DEFAULT_GAS_PRICE_NUMBER = DEFAULT_GAS_PRICE[0].amount;
+export const COSMOS_RPC = 'https://mainnet-node.like.co/rpc/';
