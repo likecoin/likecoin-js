f33c6ac4f615c233a99faf764772d00c4b5f6a8f
William Chong <me@williamchong.cloud>
2021-11-16 02:14:36 +0800

    🔧 Add webpack umd build

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist/umd'),
+    filename: 'likecoin.umd.js',
+    library: 'likecoin',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    fallback: {
+      Buffer: require.resolve('buffer'),
+      crypto: require.resolve('crypto-browserify'),
+      path: require.resolve('path-browserify'),
+      util: require.resolve('util/'),
+      stream: require.resolve('stream-browserify'),
+      assert: false,
+      fs: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?(ts|js)x?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-typescript',
+            ],
+            plugins: [
+              ['@babel/plugin-transform-runtime',
+                {
+                  regenerator: true,
+                },
+              ],
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-proposal-object-rest-spread',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production',
+      IS_TESTNET: false,
+    }),
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
+};
