b9a657bc72fc44c48241267e8e6062699b121db2
William Chong <me@williamchong.cloud>
2021-11-20 01:10:06 +0800

    ✨ Add query like tx and expose tx and iscn module

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * as arweave from './arweave';
 export * as likepay from './likepay';
+export * as iscn from './iscn';
+export * as tx from './tx';
--- src/tx/index.ts
+++ src/tx/index.ts
@@ -1,7 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { assertIsBroadcastTxSuccess, SigningStargateClient } from '@cosmjs/stargate';
+import { assertIsBroadcastTxSuccess, SigningStargateClient, StargateClient } from '@cosmjs/stargate';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { OfflineSigner } from '@cosmjs/proto-signing';
+import { OfflineSigner, decodeTxRaw } from '@cosmjs/proto-signing';
+import { MsgSend } from 'cosmjs-types/cosmos/bank/v1beta1/tx';
 import BigNumber from 'bignumber.js';
 import {
   COSMOS_DENOM,
@@ -32,4 +33,38 @@ export async function sendLIKE(
   return res;
 }
 
-export default sendLIKE;
+export async function queryLIKETransaction(txHash: string) {
+  const client = await StargateClient.connect(COSMOS_RPC);
+  const tx = await client.getTx(txHash);
+  if (!tx) return null;
+  const {
+    code,
+    tx: rawTx,
+  } = tx;
+  if (code) return tx;
+  const t = decodeTxRaw(rawTx);
+  const { body } = t;
+  const { messages: rawMessages } = body;
+  const messages = rawMessages.map(((m) => {
+    const { typeUrl, value } = m;
+    if (typeUrl === '/cosmos.bank.v1beta1.MsgSend') {
+      const payloadValue = MsgSend.decode(value);
+      return {
+        typeUrl,
+        value: payloadValue,
+      };
+    }
+    return m;
+  }));
+  return {
+    ...tx,
+    code,
+    tx: {
+      ...t,
+      body: {
+        ...body,
+        messages,
+      },
+    },
+  };
+}
